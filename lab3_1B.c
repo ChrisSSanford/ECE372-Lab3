18953517056641ae304abbfd8a553603562f035b
Christopher Houseman <chouseman@email.arizona.edu>
2014-10-27 12:09:33 -0700

    setup output compare modules

--- lab3_1B.c
+++ lab3_1B.c
@@ -37,12 +37,43 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
  */
 int main(void) {
 
-    //set the PWM period by writing to the selected timery period register (PRy)
+    
+    
+
+/**********************************************/
+    T3CONbits.TCS = 0; // sets up to use internal clock 
+    T3CONbits.TGATE = 0; 
+    T3CONbits.TON = 0;  // Turn timer 3 off
+    IFS0bits.T3IF = 0;  // reset timer 3 interrupt flag
+    TMR3 = 0;           // resets timer 3 to 0
+
+    T3CONbits.TCKPS = 11; // set a prescaler of 8 for timer 2
+    PR3 = 287;
+    IEC0bits.T3IE = 1;
+
+/*****************************************************/
+    
+
+    OC1CONbits.OCM0 = 1; // Initialize OCx pin low, compare event forces OCx pin high,
+    OC1CONbits.OCTSEL = 1; // using timer 3
+
+    OC1R = OC1RS = PR3/2;
+
+    OC2CONbits.OCM0 = 1; // Initialize OCx pin low, compare event forces OCx pin high,
+    OC2CONbits.OCTSEL = 1; // using timer 3
+
+    OC2R = OC2RS = PR3/2;
 
 
 
 
 
     return (EXIT_SUCCESS);
+}
+
+void __attribute__((interrupt,auto_psv)) _T3Interrupt(void){
+    IFS0bits.T3IF = 0;
+
+
 }
 
