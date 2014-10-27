c9a5dc7345b48ff35cd135f68d1d6a8af36b3914
Rachel Powers <rpowers6353@gmail.com>
2014-10-27 12:27:21 -0700

    stuff with duty cycle

--- lab3_1B.c
+++ lab3_1B.c
@@ -38,7 +38,9 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
 int main(void) {
 
     
-    
+    int percent = 0;
+    int duty1 = 0;
+    int duty2 = 0;
 
 /**********************************************/
     T3CONbits.TCS = 0; // sets up to use internal clock 
@@ -65,10 +67,58 @@ int main(void) {
     OC2R = OC2RS = PR3/2;
 
 
+int ADC_value;      // variable to store the binary value in the ADC buffer
+    char value[8];      //  character array to store the values to be printed to the LCD
+    double AD_value;    // variable to store the calculated value of the voltage
 
+    LCDInitialize();  // initialize the LCD display
 
+    AD1PCFG &= 0xFFDF; // Pin 7, AN5, where the POT is connected, IO6, is set to analog mode, AD module samples pin voltage
+    AD1CON2 = 0x0;       // Always uses MUX A input multiplexer settings, configured as one 16-word buffer, interrupts at the completion of conversion for each sample/convert sequence, use the channel selected by the CH0SA bits as the MUX A input
+    AD1CON3 = 0x0101;      //set the A/D conversion clock period to be 2*Tcy, set the Auto-Sample Time bits to be 1 T_AD, A/D conversion clock derived from system clock
+    AD1CON1 = 0x20E4;   // A/D sample auto-start mode set for sampling begins immediately after last conversion completes, SAMP bit is automatically set, Conversion trigger source set to internal counter (auto-convert), data output format is integer, stop in idle mode set to discontinue module operation when device enters idle mode
+    AD1CHS = 5;         // positive input is AN5
+    AD1CSSL = 0;        // low reference set to 0
 
-    return (EXIT_SUCCESS);
+    AD1CON1bits.ADON = 1; // A/D operating mode set to A/D converter module is operating
+    IFS0bits.AD1IF = 0;   // clear the A/D 1 interrupt flag
+
+    while(1)
+    {
+        while(!IFS0bits.AD1IF);  // wait while the A/D 1 interrupt flag is low
+        IFS0bits.AD1IF = 0;     // clear the A/D 1 interrupt flag
+        ADC_value = ADC1BUF0;   // stores the current value in the A/D 1 buffer in the ADC_value variable
+        sprintf(value, "%6d", ADC_value); // formats value in ADC_value as a 6 character string and stores in in the value character array
+        LCDMoveCursor(0,0);                 // moves the cursor on the LCD to the home position
+        LCDPrintString(value);              // sends value to the LCD print function to display it on the LCD screen
+        AD_value = (ADC_value * 3.3)/1024;  // converts the binary value of the voltage to the analog value by multiplying by the maximum voltage and dividing by 2^n = 2^10, then stores it in AD_value
+        sprintf(value, "%6.2f", AD_value);  // formats the value in AD_value to 2 decimals places and stores it in the value variable
+        LCDMoveCursor(1,0);                 // moves the cursor on the LCD to the second line
+        LCDPrintString(value);              // sends value to the LCD print function to display it on the LCD screen
+        if (AD_value<1.65){
+            OC1RS = PR3;
+            percent=AD_value/1.65;
+            OC2RS =PR3*percent;
+        }
+        else if (AD_value>1.65) {
+            OC2RS = PR3;
+            percent=(AD_value-1.65)/1.65;
+            OC1RS =PR3*(1-percent);
+        }
+        else {
+            OC1RS=PR3;
+            OC2RS=PR3;
+        }
+        duty1=OC1RS;
+        duty2=OC2RS;
+        sprintf(value, "%6d", duty1); // formats value in ADC_value as a 6 character string and stores in in the value character array
+        LCDMoveCursor(1,0);                 // moves the cursor on the LCD to the second line
+        LCDPrintString(value);              // sends value to the LCD print function to display it on the LCD screen
+        sprintf(value, "%6d", duty2); // formats value in ADC_value as a 6 character string and stores in in the value character array
+        LCDMoveCursor(1,4);                 // moves the cursor on the LCD to the second line
+        LCDPrintString(value);              // sends value to the LCD print function to display it on the LCD screen
+    }
+return 0;
 }
 
 void __attribute__((interrupt,auto_psv)) _T3Interrupt(void){
